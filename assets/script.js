1eac7ce894c0ab3872210de7d9438a472f00b7d7
sunnymoksoton <ylm1e25@soton.ac.uk>
2026-08-05 22:29:20 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -33,7 +33,7 @@ function isEmail(v) { return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v); }
 submitBtn.addEventListener("click", async function () {
   const email = emailEl.value.trim();
   const name  = nameEl.value.trim();
-  const data = JSON.stringify({ name: name, email: email, course: "森林浴 EEC 2026 Q3" });
+  const data = { name, email };
   
   if (!isEmail(email)) {
     emailEl.focus();
