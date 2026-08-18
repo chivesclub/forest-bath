92a6b66ab8fd6079f7cc20e6948f6999771273a9
khakitoad <117ysc@gmail.com>
2026-08-18 15:05:28 +0800

    Update send-email.js

--- jobs/send-email.js
+++ jobs/send-email.js
@@ -14,3 +14,20 @@ export async function sendEmail(email, fileName, attachmentFolder) {
     console.error('Network error:', error);
   }
 }
+
+export async function sendCustomEmail(email, htmlContent) {
+   try {
+    // Replace with your actual deployed Vercel domain URL
+    const response = await fetch('https://forest-bath-eight.vercel.app/api/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email, customHTML: htmlContent }),
+    });
+
+    const data = await response.json();
+  } catch (error) {
+    console.error('Network error:', error);
+  }
+}
